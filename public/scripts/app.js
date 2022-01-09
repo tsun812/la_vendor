645c44ce2a5c6a087f7f44b160ca9cfbfde5566b
mattdnkim <nyukcanada@gmail.com>
2022-01-08 21:20:34 -0700

    frontend work:adding photos, navigation bar, css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,38 @@
-// Client facing scripts here
+$(document).ready(function() {
+ const descContainer = $("#description-container")
+
+ $("#monalisa").click(function () {
+  if ( descContainer.first().is( ":hidden" ) ) {
+    descContainer.slideDown( "slow" );
+  } else {
+    descContainer.hide();
+  }
+});
+  })
+
+
+  /*
+
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $(".btn1").click(function(){
+    $("p").slideUp();
+  });
+  $(".btn2").click(function(){
+    $("p").slideDown();
+  });
+});
+</script>
+</head>
+<body>
+  */
+
+
+
+
+
+
+
