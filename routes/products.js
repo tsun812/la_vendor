9b37b1af2ee3bf1a9ea0a14e2ef4cec43d296444
mattdnkim <nyukcanada@gmail.com>
2022-01-12 16:26:27 -0700

    create delete route which delte data from database with delete button

---
+++ routes/products.js
@@ -0,0 +1,44 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const cookieSession = require('cookie-session');
+const app = express();
+app.use(cookieSession({
+  name: 'session',
+  keys: ["abc", "def"],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+module.exports = (db) => {
+  router.post("/delete/:id", (req, res) => {
+    //sql query to delete a record with id
+   const id = req.params.id;
+   console.log("+++++++++++++", id)
+     const querystring = `
+    DELETE
+    FROM products
+    WHERE id = $1
+    `
+    db.query(querystring, [id])
+    .then(result => {
+
+      return res.redirect("/")
+    })
+    .catch((e) => {
+      return res.status(403).send("error occurs") }
+    )
+  });
+
+
+
+
+    return router;
+  }
+
--- server.js
+++ server.js
@@ -39,7 +39,9 @@ app.use(cookieSession({
 }));
 app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
+const productsRoutes = require("./routes/products");
 app.use("/home", usersRoutes(db));
+app.use("/products", productsRoutes(db));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 
@@ -82,44 +84,44 @@ app.get("/favourites", (req, res) => {
 // ------------------------------------ app.post ------------------------------
 
 
-app.post("/sell_an_item/upload", (req, res) => {
-  const title = req.body.title
-  const price = req.body.price
-  const description = req.body.description
-  const url = req.body.url
-  const queryString = `
-  INSERT INTO products (title, price, description,url_photo)
-  VALUES ($1, $2, $3, $4)
-  RETURNING *;
-  `;
-  const values = [title, Number(price), description, url];
-    db.query(queryString, values)
-    .then(result => {
-       res.redirect("/")
-    })
-    .catch((e) => {
-      res.status(403).send("error occurs")
-    }
-   )
-  })
+// app.post("/sell_an_item/upload", (req, res) => {
+//   const title = req.body.title
+//   const price = req.body.price
+//   const description = req.body.description
+//   const url = req.body.url
+//   const queryString = `
+//   INSERT INTO products (title, price, description,url_photo)
+//   VALUES ($1, $2, $3, $4)
+//   RETURNING *;
+//   `;
+//   const values = [title, Number(price), description, url];
+//     db.query(queryString, values)
+//     .then(result => {
+//        res.redirect("/")
+//     })
+//     .catch((e) => {
+//       res.status(403).send("error occurs")
+//     }
+//    )
+//   })
 
-  app.post("/delete", async(req, res) => {
+  // app.post("/delete", async(req, res) => {
 
 
-    const querystring = `
-    DELETE
-    FROM products
-    WHERE id = 1
-    `
-    db.query(querystring)
-    .then(result => {
+  //   const querystring = `
+  //   DELETE
+  //   FROM products
+  //   WHERE id = 1
+  //   `
+  //   db.query(querystring)
+  //   .then(result => {
 
-      res.redirect("/")
-    })
-    .catch((e) => {
-      res.status(403).send("error occurs") }
-    )
-   })
+  //     res.redirect("/")
+  //   })
+  //   .catch((e) => {
+  //     res.status(403).send("error occurs") }
+  //   )
+  //  })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
