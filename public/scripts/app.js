ee146eda3c46282dfae55ec32e20009649adefac
mattdnkim <nyukcanada@gmail.com>
2022-01-13 13:24:03 -0700

    moved the uploading route to the products.js from server.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,129 +1,4 @@
 /*
-$(document).ready(function() {
-  const monalisaContainer = $("#monalisa-container")
-  const campiContainer = $("#campi-container")
-  const faceContainer = $("#face-container")
-  const lastDanceContainer = $("#last_dance-container")
-  const geniusContainer = $("#genius-container")
-  const festivalContainer = $("#festival-container")
-  const womenContainer = $("#women-container")
-  const kidBearContainer = $("#kid_bear-container")
-  const textBox = $("#text-box")
-  const submit = $("#submit")
-
-  $("#monalisa").click(function(event) {
-    event.preventDefault()
-    if (monalisaContainer.first().is(":hidden")) {
-      monalisaContainer.show("slow", () => {
-      });
-      monalisaContainer.text("Artwork: Monalisa")
-      monalisaContainer.append(textBox)
-      monalisaContainer.append(submit)
-    } else {
-      monalisaContainer.hide();
-    }
-  })
-
-  $("#campi").click(function(event) {
-    event.preventDefault()
-    if (campiContainer.first().is(":hidden")) {
-      campiContainer.show("slow", () => {
-      });
-      campiContainer.text("Artwork: Campi")
-      campiContainer.append(textBox)
-      campiContainer.append(submit)
-    } else {
-      campiContainer.hide();
-    }
-  })
-
-  $("#face").click(function(event) {
-    event.preventDefault()
-    if (faceContainer.first().is(":hidden")) {
-      faceContainer.show("slow", () => {
-      });
-      faceContainer.text("Artwork: Face")
-      faceContainer.append(textBox)
-      faceContainer.append(submit)
-    } else {
-      faceContainer.hide();
-    }
-  })
-*/
-/*
-Adding the description for the rest of the pcitures
-Jan. 10 @ 7:00 PST
-*/
-/*
-  $("#last-dance").click(function(event) {
-    event.preventDefault()
-    if (lastDanceContainer.first().is(":hidden")) {
-      lastDanceContainer.show("slow", () => {
-      });
-      lastDanceContainer.text("Artwork: Last Dance")
-      lastDanceContainer.append(textBox)
-      lastDanceContainer.append(submit)
-    } else {
-      lastDanceContainer.hide();
-    }
-  })
-
-  $("#genius").click(function(event) {
-    event.preventDefault()
-    if (geniusContainer.first().is(":hidden")) {
-      geniusContainer.show("slow", () => {
-      });
-      geniusContainer.text("Artwork: Genius - A. E.")
-      geniusContainer.append(textBox)
-      geniusContainer.append(submit)
-    } else {
-      geniusContainer.hide();
-    }
-  })
-
-  $("#festival").click(function(event) {
-    event.preventDefault()
-    if (festivalContainer.first().is(":hidden")) {
-      festivalContainer.show("slow", () => {
-      });
-      festivalContainer.text("Artwork: Festival in Peru")
-      festivalContainer.append(textBox)
-      festivalContainer.append(submit)
-    } else {
-      festivalContainer.hide();
-    }
-  })
-
-  $("#women").click(function(event) {
-    event.preventDefault()
-    if (womenContainer.first().is(":hidden")) {
-      womenContainer.show("slow", () => {
-      });
-      womenContainer.text("Artwork: Festival in Peru")
-      womenContainer.append(textBox)
-      womenContainer.append(submit)
-    } else {
-      womenContainer.hide();
-    }
-  })
-
-  $("#kid-bear").click(function(event) {
-    event.preventDefault()
-    if (kidBearContainer.first().is(":hidden")) {
-      kidBearContainer.show("slow", () => {
-      });
-      kidBearContainer.text("Artwork: Festival in Peru")
-      kidBearContainer.append(textBox)
-      kidBearContainer.append(submit)
-    } else {
-      kidBearContainer.hide();
-    }
-  })
-
-})
-*/
-
-/*
 This will traverse to the closest parent (product-container)
 and traverse back down to closest child (product-info)
 
@@ -135,17 +10,16 @@ and traverse back down to closest child (product-info)
 */
 const container = $(".container");
 //const row = $(".row");
-  const addProductToList = function(product) {
-    $(".row:last").append(product);
-    
-}
-  const createProduct = function(product, user, index, showDelete) {
-    //console.log(product);
-    const deleteButton = `
+const addProductToList = function (product) {
+  $(".row:last").append(product);
+};
+const createProduct = function (product, user, index, showDelete) {
+  //console.log(product);
+  const deleteButton = `
     <form method="post" action="/products/delete/${product.id}">
       <button id="delete-monalisa" type="submit">delete</button>
     </form>`;
-    let productHTML = `
+  let productHTML = `
     <div class="col-md-3 border">
       <ul class="product-container">
         <div class="container-image">
@@ -161,64 +35,60 @@ const container = $(".container");
         </div>
       </ul>
     </div>
-    `
-    let user_id = parseInt(user);
-    let curr_id = parseInt(product.user_id);
-    console.log(curr_id);
-    console.log(user_id);
-    addProductToList(productHTML);
-    if(user_id === curr_id) {
-      console.log("here");
-      const containerImage = $(".container-image:last");
-      containerImage.append('<button type="button">Sold</button>');
+    `;
+  let user_id = parseInt(user);
+  let curr_id = parseInt(product.user_id);
+  console.log(curr_id);
+  console.log(user_id);
+  addProductToList(productHTML);
+  if (user_id === curr_id) {
+    console.log("here");
+    const containerImage = $(".container-image:last");
+    containerImage.append('<button type="button">Sold</button>');
+  }
+};
+function addProducts(products, user) {
+  //console.log(products);
+  for (let i = 0; i < products.length; i++) {
+    if (i % 4 === 0) {
+      container.append(`<div class="row">`);
+    }
+    //console.log(products[i]);
+    //console.log(rowNum);
+    const productHTML = createProduct(products[i], user, i);
+    //addProductToList(productHTML);
+    if (i % 4 === 3) {
+      container.append(`</div>`);
     }
   }
-  function addProducts(products, user){
-    //console.log(products);
-    for (let i = 0; i < products.length; i++) {
-      if (i % 4 === 0){ 
-        container.append(`<div class="row">`);
-      }
-      //console.log(products[i]);
-      //console.log(rowNum);
-      const productHTML = createProduct(products[i], user, i);
-      //addProductToList(productHTML);
-      if (i % 4 === 3) {
-        container.append(`</div>`)
-      }
+}
 
-    }
-  }
-  
-  //this function returns current user id
-  function getCurrUserId(data){
-   return data[0].user_id;
-  }
-  function getAllListings() {
-    let url = "/getlists";
-    return $.ajax({url: url, method: 'GET'});
-  }
+//this function returns current user id
+function getCurrUserId(data) {
+  return data[0].user_id;
+}
+function getAllListings() {
+  let url = "/getlists";
+  return $.ajax({ url: url, method: "GET" });
+}
 
-  function getUser() {
-    let url = "/getUser";
-    return $.ajax({url: url, method: 'GET'});
-  }
-  let params = [];
-  $.when(
-  //get all paintings 
-  getAllListings().then(function( data ) {
+function getUser() {
+  let url = "/getUser";
+  return $.ajax({ url: url, method: "GET" });
+}
+let params = [];
+$.when(
+  //get all paintings
+  getAllListings().then(function (data) {
     params[0] = data;
   }),
   //get current user's id
-  getUser().then(function( user ) {
+  getUser().then(function (user) {
     params[1] = user;
   })
-  )
-  .then(
-    function(){
-    addProducts(params[0], params[1]);
-    }
-  );
+).then(function () {
+  addProducts(params[0], params[1]);
+});
 
 $(document).on("click", ".container-image", function () {
   $(".container-image").on("click", function (event) {
--- routes/products.js
+++ routes/products.js
@@ -5,36 +5,72 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-const cookieSession = require('cookie-session');
+const express = require("express");
+const router = express.Router();
+const cookieSession = require("cookie-session");
 const app = express();
-app.use(cookieSession({
-  name: 'session',
-  keys: ["abc", "def"],
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["abc", "def"],
 
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+
+// Web server config
+const PORT = process.env.PORT || 8080;
+const sassMiddleware = require("../lib/sass-middleware");
+const morgan = require("morgan");
+const database = require("../database");
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("../lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 module.exports = (db) => {
   router.post("/delete/:id", (req, res) => {
     //sql query to delete a record with id
-   const id = req.params.id;
-     const querystring = `
+    const id = req.params.id;
+    const querystring = `
     DELETE
     FROM products
-    WHERE id = $1 
-    `
+    WHERE id = $1
+    `;
     db.query(querystring, [id])
-    .then(result => {
-
-      return res.redirect("/")
-    })
-    .catch((e) => {
-      return res.status(403).send("error occurs") }
-    )
+      .then((result) => {
+        return res.redirect("/");
+      })
+      .catch((e) => {
+        return res.status(403).send("error occurs");
+      });
   });
 
-    return router;
-  }
-
+  router.post("/sell_an_item/upload", (req, res) => {
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+    const url = req.body.url;
+    const queryString = `
+      INSERT INTO products (title, price, description,url_photo)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+      `;
+    const values = [title, price, description, url];
+    db.query(queryString, values)
+      .then((result) => {
+        return res.redirect("/");
+      })
+      .catch((e) => {
+        return res.status(403).send("error occurs");
+      });
+  });
+  return router;
+};
